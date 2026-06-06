001d0ea449fe6a7bbedacc1c93a7b3eea3cacc87
masterjuster-hash <masterjuster@gmail.com>
2026-06-06 14:38:18 +0300

    Add files via upload

---
+++ local-plugins/cordova-plugin-battery-guard/www/BatteryGuard.js
@@ -0,0 +1,15 @@
+﻿var exec = require('cordova/exec');
+
+var BatteryGuard = {
+    saveSettings: function(lowLimit, highLimit, intervalMin, lowSound, highSound, success, error) {
+        exec(success, error, 'BatteryGuardPlugin', 'saveSettings', [lowLimit, highLimit, intervalMin, lowSound, highSound]);
+    },
+    stopService: function(success, error) {
+        exec(success, error, 'BatteryGuardPlugin', 'stopService', []);
+    },
+    selectSound: function(soundType, success, error) {
+        exec(success, error, 'BatteryGuardPlugin', 'selectSound', [soundType]);
+    }
+};
+
+module.exports = BatteryGuard;
