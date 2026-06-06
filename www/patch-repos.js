4a06b86d8b9e8e119473fa95f92d34ca3bced251
masterjuster-hash <masterjuster@gmail.com>
2026-06-06 18:25:58 +0300

    Add files via upload

---
+++ www/patch-repos.js
@@ -0,0 +1,33 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+module.exports = function(context) {
+    const platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+    if (!fs.existsSync(platformRoot)) return;
+
+    function walk(dir) {
+        let results = [];
+        const list = fs.readdirSync(dir);
+        list.forEach(file => {
+            file = path.join(dir, file);
+            const stat = fs.statSync(file);
+            if (stat && stat.isDirectory()) {
+                results = results.concat(walk(file));
+            } else {
+                if (file.endsWith('.gradle')) results.push(file);
+            }
+        });
+        return results;
+    }
+
+    try {
+        const gradleFiles = walk(platformRoot);
+        gradleFiles.forEach(file => {
+            let content = fs.readFileSync(file, 'utf8');
+            if (content.includes('jcenter()')) {
+                content = content.replace(/jcenter\(\)/g, 'mavenCentral()');
+                fs.writeFileSync(file, content, 'utf8');
+            }
+        });
+    } catch (err) {}
+};
