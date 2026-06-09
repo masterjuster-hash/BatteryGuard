c07e408f1b90a78cd8a46d2e122ae06e3302ecad
masterjuster-hash <masterjuster@gmail.com>
2026-06-09 15:13:11 +0300

    Add files via upload

--- www/patch-repos.js
+++ www/patch-repos.js
@@ -24,32 +24,52 @@ module.exports = function(context) {
         const gradleFiles = walk(platformRoot);
         gradleFiles.forEach(file => {
             let content = fs.readFileSync(file, 'utf8');
-            let lines = content.split('\n');
             let changed = false;
 
-            let newLines = lines.filter(function(line) {
-                // Вырезаем импорт и строку зависимости под корень
-                if (line.indexOf('versioncompare') !== -1) {
-                    changed = true;
-                    return false;
-                }
-                return true;
-            });
+            // 1. Если это основной скрипт cordova.gradle, лечим вызовы класса Version
+            if (file.endsWith('cordova.gradle')) {
+                let lines = content.split('\n');
+                let newLines = lines.map(function(line) {
+                    // Убираем импорт
+                    if (line.indexOf('com.g00fy2.versioncompare.Version') !== -1) {
+                        return '// ' + line;
+                    }
+                    // Глушим проверку на версию 0.0.0 (строка 43)
+                    if (line.indexOf('isEqual(\'0.0.0\')') !== -1) {
+                        return '    return true; // Patched';
+                    }
+                    // Глушим сортировку версий (строка 57)
+                    if (line.indexOf('.collect { new Version(it) }') !== -1) {
+                        return '        .collect { it } // Patched';
+                    }
+                    return line;
+                });
+                content = newLines.join('\n');
+                changed = true;
+            }
 
-            let finalContent = newLines.join('\n');
+            // 2. Вырезаем любые остаточные упоминания зависимости из build.gradle
+            if (content.indexOf('com.g00fy2:versioncompare') !== -1) {
+                let lines = content.split('\n');
+                let filteredLines = lines.filter(function(line) {
+                    return line.indexOf('com.g00fy2:versioncompare') === -1;
+                });
+                content = filteredLines.join('\n');
+                changed = true;
+            }
 
-            // Заменяем репозитории
-            if (finalContent.indexOf('jcenter()') !== -1) {
-                finalContent = finalContent.split('jcenter()').join('mavenCentral()');
+            // 3. Заменяем мертвый jcenter на mavenCentral
+            if (content.indexOf('jcenter()') !== -1) {
+                content = content.split('jcenter()').join('mavenCentral()');
                 changed = true;
             }
 
             if (changed) {
-                fs.writeFileSync(file, finalContent, 'utf8');
-                console.log('Patched file: ' + path.basename(file));
+                fs.writeFileSync(file, content, 'utf8');
+                console.log('Successfully patched: ' + path.basename(file));
             }
         });
     } catch (err) {
-        console.error('Error in hook: ' + err);
+        console.error('Error in infrastructure hook: ' + err);
     }
 };
