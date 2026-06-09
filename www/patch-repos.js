e5cafbc60f693035f613909aac2d0a0c71e2099f
masterjuster-hash <masterjuster@gmail.com>
2026-06-09 18:07:36 +0300

    Add files via upload

--- www/patch-repos.js
+++ www/patch-repos.js
@@ -9,18 +9,16 @@ module.exports = function(context) {
         return;
     }
 
-    console.log('--- [Hook] Starting Local-Dependency injection patcher...');
+    console.log('--- [Hook] Starting Fixed-Dependency injection patcher...');
 
     const jarUrl = 'https://repo1.maven.org/maven2/com/g00fy2/versioncompare/1.3.4/versioncompare-1.3.4.jar';
     const libsDir = path.join(platformRoot, 'libs');
     const jarPath = path.join(libsDir, 'versioncompare-1.3.4.jar');
 
-    // Создаем папку libs в сгенерированном Android проекте, если её нет
     if (!fs.existsSync(libsDir)) {
         fs.mkdirSync(libsDir, { recursive: true });
     }
 
-    // Скачиваем JAR-ник напрямую из живого Maven Central во время сборки
     console.log('--- [Hook] Downloading versioncompare.jar from Maven Central...');
     
     const file = fs.createWriteStream(jarPath);
@@ -58,13 +56,12 @@ module.exports = function(context) {
                 let content = fs.readFileSync(file, 'utf8');
                 let changed = false;
 
-                // Вместо удаления зависимости, мы говорим искать её в локальной папке libs
+                // ИСПРАВЛЕНО: Никаких BonaparteRoot, только чистый стандартный путь Gradle
                 if (content.indexOf('repositories {') !== -1 && content.indexOf('flatDir') === -1) {
-                    content = content.split('repositories {').join('repositories {\n        flatDir { dirs BonaparteRoot + "/libs" }\n        flatDir { dirs "${project.rootDir}/libs" }\n        mavenCentral()');
+                    content = content.split('repositories {').join('repositories {\n        flatDir { dirs "${project.rootDir}/libs" }\n        mavenCentral()');
                     changed = true;
                 }
 
-                // Меняем глобальный jcenter на mavenCentral везде для остальных библиотек
                 if (content.indexOf('jcenter()') !== -1) {
                     content = content.split('jcenter()').join('mavenCentral()');
                     changed = true;
